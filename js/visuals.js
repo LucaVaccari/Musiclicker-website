b610a832747988d744efd00db7256cef3e7b6024
Luca Vaccari <lucavaccari03@gmail.com>
2020-06-02 14:37:56 +0200

    Completed visuals

--- js/visuals.js
+++ js/visuals.js
@@ -1,28 +1,57 @@
-let scene, camera, light, renderer;
+import { GLTFLoader } from "../js/three.js/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "../js/three.js/examples/jsm/controls/OrbitControls.js";
 
 
-$(function(){
-	init();
+$(function() {
+	init("Cello");
+	init("Organ");
+	init("Piano");
+	init("Trumpet");
+	init("Synth");
 });
 
-function init()
-{
+function init(instrument) {
+	let scene, camera, light, renderer;
 	scene = new THREE.Scene();
 	scene.background = new THREE.Color(0x2980b9);
 
-	camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 5000);
+	camera = new THREE.PerspectiveCamera(
+		40,
+		window.innerWidth / window.innerHeight,
+		1,
+		5000,
+	);
 	light = new THREE.AmbientLight(0xecf0f1);
 
 	scene.add(light);
 
-	renderer = new THREE.WebGLRenderer({antialias: true});
-	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer = new THREE.WebGLRenderer({ 
+		antialias: true,
+		alpha: true,
+		autoClear: true,
+		physicallyCorrectLights: true,
+	 });
+	renderer.setSize(window.innerWidth / 1.1, window.innerHeight / 1.1);
 
-	document.body.appendChild(renderer.domElement);
+	document.getElementById("render").appendChild(renderer.domElement);
 
-	let loader = new THREE.GLTFLoader();
-	loader.load("../resources/3dModels/CelloModel.glb", (gltf) => {
+	let controls = new OrbitControls(camera, renderer.domElement);
+	controls.addEventListener("change", renderer);
+
+	let loader = new GLTFLoader();
+	loader.load("../resources/3dModels/" + instrument + "Model.glb", function(gltf) {
+		gltf.scene.scale.set(.5, .5, .5);
+		gltf.scene.position.x = 0;
+		gltf.scene.position.y = 0;
+		gltf.scene.position.z = 0;
+		camera.position.set(0, 0, 10);
 		scene.add(gltf.scene);
-		renderer.renderer(scene, camera);
+		animate();
 	});
+	
+	function animate()
+	{
+		renderer.render(scene, camera);
+		requestAnimationFrame(animate);
+	}
 }

681ab25f7d7b1a101922c0f950c344b5e7e348a2
Luca Vaccari <lucavaccari03@gmail.com>
2020-06-05 12:46:45 +0200

    Update code.js

--- js/code.js
+++ js/code.js
@@ -1,31 +1,61 @@
-const suffixes = ["", "K", "M", "G", "T", "P", "E", "Z", "Y", "aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh", "ii", "jj", "kk", "ll", "mm", "nn", "oo", "pp", "qq"];
+const suffixes = [
+	"",
+	"K",
+	"M",
+	"G",
+	"T",
+	"P",
+	"E",
+	"Z",
+	"Y",
+	"aa",
+	"bb",
+	"cc",
+	"dd",
+	"ee",
+	"ff",
+	"gg",
+	"hh",
+	"ii",
+	"jj",
+	"kk",
+	"ll",
+	"mm",
+	"nn",
+	"oo",
+	"pp",
+	"qq",
+];
 
 class BigNumber {
 	constructor(base = 0, exp = 1) {
 		this.base = base;
 		this.exp = exp;
 
-		while ((this.exp) % 3 != 0) {
+		while (this.exp % 3 != 0) {
 			this.exp++;
 			this.base /= 10;
 		}
 	}
 	reduce() {
-		while (this.base >= 1000)
-        {
-            this.base /= 1000;
-            this.exp += 3;
-        }
+		while (this.base >= 1000) {
+			this.base /= 1000;
+			this.exp += 3;
+		}
 	}
 	toString() {
 		this.reduce();
-		if(suffixes[parseInt(this.exp == 0 ? 0 : (this.exp + 2) / 3)] == undefined)
-		{
-			console.log(this.exp == 0 ? 0 : (this.exp + 2) / 3);
+		if (
+			suffixes[parseInt(this.exp == 0 ? 0 : (this.exp + 2) / 3)] ==
+			undefined
+		) {
 			return "inf";
 		}
-		
-		return this.base + suffixes[parseInt(this.exp == 0 ? 0 : (this.exp + 2) / 3)]; //add suffix
+
+		return (
+			this.base.toFixed(2) +
+			suffixes[parseInt(this.exp == 0 ? 0 : (this.exp + 2) / 3)]
+		); //add suffix
 	}
 }
 
